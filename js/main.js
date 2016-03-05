776fd963cbbe4efc186dfed9e3ddb45f65b51c6c
Matthew Stratton <development@mmstratton.com>
2016-03-04 19:11:25 -0500

    generating a row on button click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function() {
+    $("#generate-character").click(function(){
+      var row_html='<tr>' +
+        '<th class="col-xs-2">#</th>' +
+        '<th class="col-xs-4">Name</th>' +
+        '<th class="col-xs-4">Type</th>' +
+        '<th class="col-xs-2">HP</th>' +
+        '<th class="col-xs-8">Gear</th>' +
+        '</tr>';
+        $('#character-table tr:last').after(row_html);
+    }); 
+});
